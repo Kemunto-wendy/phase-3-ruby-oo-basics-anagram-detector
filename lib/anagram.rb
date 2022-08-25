be799bb663973ac65b3024c2068d91945418afab
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-08-25 20:36:14 +0300

    changes

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :name
+
+   def initialize(word)
+    @name = word
+    puts word
+  end
+
+  def match array
+    array.select {|word| word.chars.sort ==@name.chars.sort}
+end
+end
